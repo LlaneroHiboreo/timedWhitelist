65a3d7bbda108d639c2a7d851319a163de63a576
LlaneroHiboreo <joan.gaztelu.camps.3@gmail.com>
2022-10-31 19:25:14 +0100

    upload upkeeep

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,7 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Whitelist{
+// imports
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+contract Whitelist is KeeperCompatibleInterface{
     // Address to receive payments after time or account limit exceeds
     address payable public immutable feeAccount;
 
@@ -14,7 +17,8 @@ contract Whitelist{
     // Amount fee to enter whitelist
     uint256 public feeAmount;
     // variable to store deploy date
-    uint256 deployDate;
+    uint lastTimeStamp;
+    uint interval;
 
     // Mapping to keep track of saved addresses
     mapping(address => bool) public whitelistedAddresses;
@@ -24,15 +28,16 @@ contract Whitelist{
     event PaidAdded(address indexed _user, uint8 _numAddressesWhitelisted);
 
     //  Constructor, set max number of adrs
-    constructor(uint8 _maxWhitelistedAddresses, uint256 _feeAmount){
+    constructor(uint8 _maxWhitelistedAddresses, uint256 _feeAmount, uint _interval){
         maxWhitelistedAddresses = _maxWhitelistedAddresses;
         feeAmount=_feeAmount;
-        deployDate=block.timestamp;
+        lastTimeStamp=block.timestamp;
         feeAccount=payable(msg.sender);
+        interval=_interval;
     }
 
     // function to add address free
-    function _freeAddAddressToWhitelist() internal{
+    function _AddAddressToWhitelist() internal{
         // require address is not already listed
         require(!whitelistedAddresses[msg.sender], "Address Already Listed :/");
 
@@ -44,34 +49,33 @@ contract Whitelist{
 
         // update counter
         numAddressesWhitelisted++;
-
-        // emit event
-        emit Added(msg.sender, numAddressesWhitelisted);
-    }
-
-    function _paidAddAddressToWhitelist() internal{
-        // require address is not already listed
-        require(!whitelistedAddresses[msg.sender], "Address Already Listed :/");
-        require(numAddressesWhitelisted < maxWhitelistedAddresses, "Max Number of Addresses Achieved :(");
-        
-        // add address to mapping
-        whitelistedAddresses[msg.sender] = true;
-
-        // update counter
-        numAddressesWhitelisted++;
-
-        // emit event
-        emit PaidAdded(msg.sender, numAddressesWhitelisted);
     }
 
     // Function to add address
     function addAddressToWhitelist() public payable{
-        if((block.timestamp <= (deployDate + 5 minutes))){
-            _freeAddAddressToWhitelist();
+        if(feeAmount == 0){
+            _AddAddressToWhitelist();
+            // emit event
+            emit Added(msg.sender, numAddressesWhitelisted);
         }else{
             feeAccount.transfer(feeAmount);
-            _paidAddAddressToWhitelist();
+            _AddAddressToWhitelist();
+            // emit event
+            emit PaidAdded(msg.sender, numAddressesWhitelisted);
         }
     }
 
+    function checkUpkeep(bytes calldata /* checkData */) external override returns (bool upkeepNeeded, bytes memory /* performData */) {
+        // check if enough time have passed to update price
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+    }
+    
+    function performUpkeep(bytes calldata /* performData */) external override {
+        //We highly recommend revalidating the upkeep in the performUpkeep function
+        if ((block.timestamp - lastTimeStamp) > interval ) {
+            lastTimeStamp = block.timestamp;
+            // increase fee price to register
+            feeAmount = feeAmount + (2 ether);
+        }
+    }
 }
